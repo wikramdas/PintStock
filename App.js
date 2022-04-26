8805d576377852199d9b51d1c5f38337dbb3961e
Wikram Das <wikramdas0@gmail.com>
2022-04-26 08:49:58 +0500

    Task completed

--- App.js
+++ App.js
@@ -1,112 +1,181 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
+  FlatList,
+  Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  ActivityIndicator,
 } from 'react-native';
+import FIcons from 'react-native-vector-icons/Feather';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const apiUrl =
+  'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=demo';
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [dataArray, setData] = useState([]);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  useEffect(() => {
+    let mounted = true;
+    fetch(apiUrl)
+      .then(results => results.text())
+      .then(response => {
+        const jsonData = csvToJSON(response);
+        setLoading(false);
+        setData(jsonData);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+    return () => (mounted = false);
+  }, []);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const csvToJSON = csv => {
+    var lines = csv.split('\n');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    var result = [];
+
+    var headers = lines[0].split(',');
+
+    for (var i = 1; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split(',');
+
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+    }
+    return result;
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          size={'large'}
+          color={'#40C7C0'}
+        />
+      ) : (
+        <FlatList
+          data={dataArray}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <View key={index.toString()}>
+                <View style={styles.itemContainer}>
+                  <View
+                    style={{
+                      flex: 1,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <Image
+                      resizeMode="contain"
+                      style={styles.image}
+                      source={{
+                        uri: `https://storage.googleapis.com/iex/api/logos/${item?.symbol}.png`,
+                      }}
+                    />
+                  </View>
+                  <View
+                    style={{
+                      flex: 4,
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <View style={{flex: 3, marginHorizontal: 10}}>
+                      <Text style={styles.headingText}>{item?.symbol}</Text>
+                      <Text
+                        numberOfLines={1}
+                        style={{
+                          fontSize: 16,
+                        }}>
+                        {item?.name}
+                      </Text>
+                    </View>
+                    <View
+                      style={{
+                        flex: 2,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}>
+                      <Text
+                        style={{
+                          ...styles.headingText,
+                          flex: 1,
+                        }}>
+                        ${item?.price}
+                      </Text>
+                      <View
+                        style={{
+                          flex: 1,
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                        }}>
+                        <View
+                          style={{
+                            flexDirection: 'row',
+                            marginHorizontal: 10,
+                            alignItems: 'center',
+                          }}>
+                          <FIcons name="arrow-down" size={14} color={'red'} />
+                          <Text style={styles.warningText}>
+                            {item?.lowPrice}
+                          </Text>
+                        </View>
+                        <Text style={styles.warningText}>{item?.high}%</Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+                <View style={styles.separator} />
+              </View>
+            );
+          }}
+        />
+      )}
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 400 / 2,
+    borderWidth: 4,
+    borderColor: '#eee',
+    margin: 5,
+  },
+  headingText: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: 'bold',
   },
-  highlight: {
-    fontWeight: '700',
+  separator: {
+    marginVertical: 8,
+    borderWidth: 0.7,
+    borderColor: '#eee',
+  },
+  warningText: {
+    color: 'red',
   },
 });
-
 export default App;
